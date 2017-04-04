b1c62eb3299a06c135615b18f78d9b33b39929b3
Dan Clarke <accounts.github@overbythere.co.uk>
2017-04-04 13:53:11 +0100

    Min-height on board, Removed text from buttons
    Added a minimum height to the kanboard display, minimum of the height
    of the window, meaning you won’t get a scroll bar part way down the
    screen.
    Updated the icons in the switcher header to only have icons and no text.

---
+++ skin.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  if($(window).width()<767) {
+    $('#board-container').css({'min-height':($(window).height()-$('#board-container').offset().top)-10});
+    $('.views-switcher-component ul.views li a').each(function() {
+      var icon = $('i',this).clone();
+      $(this).html(icon);
+    });
+  }
+
+});
