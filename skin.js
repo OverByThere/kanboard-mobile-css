acfef9ca6b02d51dc1c9193c4ea62351c312b6d5
Dan Clarke <accounts.github@overbythere.co.uk>
2017-04-04 14:00:37 +0100

    Menu move, Filter box sizing, On resize
    Added menu to 'icon bar’.
    Updated filter box to fit.
    Added ‘on resize’ board sizing fix.

--- skin.js
+++ skin.js
@@ -1,10 +1,15 @@
+function resizeBoard() {
+  $('#board-container').css({'min-height':($(window).height()-$('#board-container').offset().top)-10});
+}
 $(document).ready(function() {
   if($(window).width()<767) {
-    $('#board-container').css({'min-height':($(window).height()-$('#board-container').offset().top)-10});
     $('.views-switcher-component ul.views li a').each(function() {
       var icon = $('i',this).clone();
       $(this).html(icon);
     });
+    resizeBoard();
   }
-
+});
+$(window).on('resize',function() {
+  resizeBoard();
 });
